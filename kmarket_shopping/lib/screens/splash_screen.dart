ee21ddb7b395d364e9e096cf33463e1a773cd42b
sub6550 <sub6550@gmail.com>
2025-10-30 09:52:33 +0900

    3차 미니 프로젝트 - Kmarket 앱 개발

---
+++ kmarket_shopping/lib/screens/splash_screen.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('images/logo.png'),
+            const SizedBox(height: 20,),
+            CircularProgressIndicator()
+          ],
+        ),
+      ),
+    );
+  }
+}
